a1b1d278e7770cb28280b46e739382b3edbedb95
Tushar Chopra <tusharchopratech.com>
2019-05-23 17:42:30 -0400

    communication time 4ms average

--- wearablecode/usb_1608/scripts/run.sh
+++ wearablecode/usb_1608/scripts/run.sh
@@ -1,5 +1,5 @@
 cd ../bin
-g++ ../src/main.cpp ../src/modules/m_daq/MDaq.cpp -L/../lib -luldaq
+g++ ../src/main.cpp ../src/modules/m_daq/MDaq.cpp -L/../lib -luldaq -std=c++0x
 if [ $? -eq 0 ]; then
     printf "\nCompiled Successfully!\n\n"
     ./a.out
